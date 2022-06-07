f6b8d382433cdfe290df51590540bbeeaa81a2c3
guinetik <guinetik@gmail.com>
2022-06-07 20:44:53 -0300

    zsh configs

--- init5.sh
+++ init5.sh
@@ -17,27 +17,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 #
 printf "copying config files...\n"
+mkdir -p ~/.config && touch ~/.config/starship.toml
 cp ./zsh/.zshrc ~/.zshrc
 cp ./zsh/nerd-font-symbols.toml ~/.config/starship.toml
 #
-printf "insntalling docker repository software..."
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-#
-printf "Add Docker official GPG key..."
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#
-printf "setting up docker repository"
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#
 printf "installing docker..."
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker
 #
 printf "installing docker compose...\n"
 DOCKER_COMPOSE_DESTINATION=/usr/local/bin/docker-compose
